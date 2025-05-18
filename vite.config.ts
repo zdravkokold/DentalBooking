c010ef62f5480e23fc002012e347548949d390af
Zdravko Koldzhiev <zdravko.koldzhiev-ext@hpe.com>
2025-05-18 19:51:17 +0300

    Calendar

--- vite.config.ts
+++ vite.config.ts
@@ -6,8 +6,8 @@ import { componentTagger } from "lovable-tagger";
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   server: {
-    host: "::",
-    port: 8080,
+    port: 3000,
+    open: true, // This will automatically open the browser
   },
   plugins: [
     react(),
